045a48bec8e2b8de11644c3560f6a9d2807c1306
pablopachecoo <power.xone31@gmail.com>
2020-01-17 11:08:48 -0300

    Desafio 04 da prova em C#

--- br.com.gft.main/main.cs
+++ br.com.gft.main/main.cs
@@ -13,13 +13,41 @@ namespace Ex04cs.br.com.gft.main
             Supervisor supervisor = new Supervisor("Fernando", 32, 5000);
             Vendedor vendedor = new Vendedor("Vault", 27, 2000);
 
-            Console.WriteLine("===============================|");
-            Console.WriteLine("Nome gerente: " + gerente.nome + "            |" + "\n" + "Idade gerente: " + gerente.idade + "              |" +  "\n" + "Gerente salario: " + gerente.bonificacao() + "         "+        "|");
-            Console.WriteLine("===============================|");
-            Console.WriteLine("Nome supervisor: " + supervisor.nome + "      |" + "\n" + "Idade supervisor: " + supervisor.idade + "           |" + "\n" + "supervisor salario: " + supervisor.bonificacao() + "      " + "|");
-            Console.WriteLine("===============================|");
-            Console.WriteLine("Nome vendedor: " + vendedor.nome + "           |" + "\n" + "Idade vendedor: " + vendedor.idade + "             |" + "\n" + "vendedor salario: " + vendedor.bonificacao() + "         |");
-            Console.WriteLine("===============================|");
+            Console.WriteLine("░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░");
+            Console.WriteLine("░Nome gerente: " + gerente.nome + "            ░" + "\n" + "░Idade gerente: " + gerente.idade + "              ░" +  "\n" + "░Gerente salario: " + gerente.bonificacao() + "         "+        "░");
+            Console.WriteLine("░───────────────────────────────░");
+            Console.WriteLine("░Nome supervisor: " + supervisor.nome + "      ░" + "\n" + "░Idade supervisor: " + supervisor.idade + "           ░" + "\n" + "░supervisor salario: " + supervisor.bonificacao() + "      " + "░");
+            Console.WriteLine("░───────────────────────────────░");
+            Console.WriteLine("░Nome vendedor: " + vendedor.nome + "           ░" + "\n" + "░Idade vendedor: " + vendedor.idade + "             ░" + "\n" + "░vendedor salario: " + vendedor.bonificacao() + "         ░");
+            Console.WriteLine("░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░");
+
+            Console.WriteLine("                         ______                    ");
+            Console.WriteLine(" _________        .---------------" + "---.              ");
+            Console.WriteLine(":______.-':      :  .--------------.  :             ");
+            Console.WriteLine("| ______  |      | :                : |             ");
+            Console.WriteLine("| ______  |      | :                : |             ");
+            Console.WriteLine("|:______B:|      | |       GFT      | |             ");
+            Console.WriteLine("|:______B:|      | |                | |             ");
+            Console.WriteLine("|:______B:|      | |                | |             ");
+            Console.WriteLine("|         |      | |                | |             ");
+            Console.WriteLine("|:_____:  |      | |                | |             ");
+            Console.WriteLine("|    ==   |      | :                : |             ");
+            Console.WriteLine("|       O |      :  '--------------'  :             ");
+            Console.WriteLine("|       o |      :'---...______...---'              ");
+
+            Console.WriteLine("|'-.____o_|   '-.   '-...______...-'  `-._          ");
+            Console.WriteLine(":_________:      `.____________________   `-.___.-. ");
+            Console.WriteLine("                 .'.eeeeeeeeeeeeeeeeee.'.      :___:");
+            Console.WriteLine("               .'.eeeeeeeeeeeeeeeeeeeeee.'.         ");
+            Console.WriteLine("              :____________________________:");
+
+
+
+
+
+
+
+
             Console.ReadLine();
         }
     }
